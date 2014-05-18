9240028afcbbb3b81c56b9104ad74af941877873
BryceHas2Bubbles <brycesimpson99@gmail.com>
2014-05-19 11:40:05 +1200

    added : to the end of: if command==(commands):

--- main.py
+++ main.py
@@ -54,13 +54,13 @@ commands={
 'laser'
 }
 
-if command==(commands)
+if command==(commands):
           print('Please wait')
           time.sleep(1.5)
           print('')
           print('Executing command' + commands)
 
-if command!=(commands)
+if command!=(commands):
           print 'Please Wait'
           sleep (1.5)
           print 'No Such Command'
