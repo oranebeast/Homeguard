70afd30ac0039c8272bd748c14ccd82d7bf0c31a
BryceHas2Bubbles <brycesimpson99@gmail.com>
2014-05-19 11:31:09 +1200

    Update and rename login.py to main.py
    Decided to put all the major scripts into one file to make it easier

--- main.py
+++ main.py
@@ -45,3 +45,22 @@ if name != (username):
         print('Incorrect, closing program.')
         time.sleep(1.5)
         exit
+
+#executes commands for the script files
+
+command=input()
+
+commands={
+'laser'
+}
+
+if command==(commands)
+          print('Please wait')
+          time.sleep(1.5)
+          print('')
+          print('Executing command' + commands)
+
+if command!=(commands)
+          print 'Please Wait'
+          sleep (1.5)
+          print 'No Such Command'
