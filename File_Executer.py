e18f07f5055d0152d3568000c6b2f815f6f582c2
BryceHas2Bubbles <brycesimpson99@gmail.com>
2014-05-19 11:10:10 +1200

    Created new file: File_Executer.py

---
+++ File_Executer.py
@@ -0,0 +1,13 @@
+#executes commands for the script files
+
+command=input()
+
+commands={
+'laser'
+}
+
+if command==(commands)
+          print('Please wait')
+          time.sleep(1.5)
+          print('')
+          print('Executing command' + commands)
