f3130d871f979a9236d417bc0ea0169ced1d1a04
oranebeast <oranebeastxboxlive@gmail.com>
2014-05-19 09:59:37 +1200

    Comments
    Woo!

--- login.py
+++ login.py
@@ -1,11 +1,13 @@
 #Login
  
 import time
- 
+  For name
 print('Please enter your username.')
  
 name=input()
  
+#the three types of users, chnage the names and the password. They act as two codes, the username is a perm based password 
+#giving the user a perm, the second is the system wide code, for any changes made
 username=  {
 'Admin'
 'User'
