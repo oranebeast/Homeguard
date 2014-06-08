daeb37734a5440536788c206bc9188ae128cb88b
BryceHas2Bubbles <brycesimpson99@gmail.com>
2014-06-09 09:57:40 +1200

    Update from the raspberry pi

--- main.py
+++ main.py
@@ -10,149 +10,136 @@
 #                               __/ |                      
 #                              |___/                 
 
-
-
-
-
-
-
-
-#init
+#import cv2
+#import sys
+#import hashlib
+#import getpass
 import time
 import logging
-import sys
+
+#Variables
+localtime = time.asctime( time.localtime(time.time()) )
+enterpassword='pass'
+version1='HomeGuard Version 1.1'
+laser='laser'
+light='light'
+upload='upload'
+opengui='opengui'
+help='help'
+egg='egg'
+exitprogram='exitprogram'
+version='version'
+       
+print('Please enter your Password.')
+password=input()
+
+if password == (enterpassword):
+        print('Please wait')
+        time.sleep(2)
+        print('')
+        print('Correct, logging in.')
+        time.sleep(1.5)
+        print('Welcome User')
+        time.sleep(2)
+#Logs people that have logged into the console
+        logging.basicConfig(filename='serverlogs.log',level=logging.DEBUG)
+        logging.debug("User logged in at " + localtime)
 
 
-#define main loop
-def _mainloop_()
-  print('Please enter your Password.')
+if password != ("pass"):
+        print('Please wait')
+        time.sleep(2)
+        print('')
+        print('Incorrect, closing program.')
+        time.sleep(1.5)
+        exit("Closed")
 
-  password=input()
+#Group Command Structure:
+#Admin
+#Mod
+#User
 
-  enterpassword='pass'
-  if password == (enterpassword):
-          print('Please wait')
-          time.sleep(2)
-          print('')
-          print('Correct, logging in.')
-          time.sleep(1.5)
-          print('Welcome User')
-          time.sleep(2)
-  #Logs people that have logged into the console
-          logging.basicConfig(filename='previouslogins.log',level=logging.DEBUG)
-          logging.debug("Logged in at")
-          logging.debug(time)
+#executes commands when entered
+print('Welcome to your HomeGuard command console by Vitanoxi')
+print('Please input your command below.')
 
+command=input()
 
-  if password != ("pass"):
-          print('Please wait')
-          time.sleep(2)
-          print('')
-          print('Incorrect, closing program.')
-          time.sleep(1.5)
-          exit()
+#Put this in to make it simple
 
-  #Group Command Structure:
-  #Admin
-  #Mod
-  #User
+if command == (laser):
+        print('Please wait')
+        time.sleep(1.5)
+        execfile("laser.py")
+        print('Executing command')
+        logging.basicConfig(filename='serverlogs.log',level=logging.DEBUG)
+        logging.debug("Someone has used the laser command " + localtime)
+        time.sleep(2)
+        command=input()
 
-  #executes commands when entered
-  print('Welcome to your HomeGuard command console by Vitanoxi')
-  print('Please input your command below.')
+if command == (light):
+        print('Please wait')
+        time.sleep(1.5)
+        execfile("light.py")
+        print('Executing command')
+        logging.basicConfig(filename='serverlogs.log',level=logging.DEBUG)
+        logging.debug("Someone has used the light command " + localtime)
+        time.sleep(2)
+        command=input()
 
-  command=input()
+if command == (upload):
+        print('Please wait')
+        time.sleep(1.5)
+        execfile("upload.py")
+        print('Executing command')
+        logging.basicConfig(filename='serverlogs.log',level=logging.DEBUG)
+        logging.debug("Someone has used the upload command "  + localtime)
+        time.sleep(2)
+        command=input()
 
-  version='HomeGuard Version 1.1'
-  creator='Vitanoxi <Vitanoxi@gmail.com>'
+if command == (opengui):
+        print('Please wait')
+        time.sleep(1.5)
+        execfile("gui.py")
+        print('Executing command')
+        logging.basicConfig(filename='serverlogs.log',level=logging.DEBUG)
+        logging.debug("Someone has used the open gui command "  + localtime)
+        time.sleep(2)
+        command=input()
 
-  laser='laser'
-  light='light'
-  upload='upload'
-  opengui='opengui'
-  help='help'
-  egg='egg'
-  exitprogram='exitprogram'
-  }
-  #Put this in to make it simple
-  if command == (creator):
-          print(version+creator)
-          time.sleep(2)
-          command=input()
+if command == (help):
+        print('admin, mod, laser, upload, opengui, help, exit, version')
+        logging.basicConfig(filename='serverlogs.log',level=logging.DEBUG)
+        logging.debug("Someone has used the help command " + localtime)
+        time.sleep(2)
+        command=input()
 
+if command == (egg):
+        print('Congrats you have found the easter egg here is your prize!')
+        time.sleep(5)
+        exit()
 
-
-  if command == (laser):
-          print('Please wait')
-          time.sleep(1.5)
-          execfile("laser.py")
-          print('Executing command')
-          logging.basicConfig(filename='Previouscommands.log',level=logging.DEBUG)
-          logging.debug(time + username + "has used the laser command")
-          time.sleep(2)
-          command=input()
-
-  if command == (light):
-          print('Please wait')
-          time.sleep(1.5)
-          execfile("light.py")
-          print('Executing command')
-          logging.basicConfig(filename='Previouscommands.log',level=logging.DEBUG)
-          logging.debug(time + username + "has used the light command")
-          time.sleep(2)
-          command=input()
-
-  if command == (upload):
-          print('Please wait')
-          time.sleep(1.5)
-          execfile("upload.py")
-          print('Executing command')
-          logging.basicConfig(filename='Previouscommands.log',level=logging.DEBUG)
-          logging.debug(time + username + "has used the upload command")
-          time.sleep(2)
-          command=input()
-
-  if command == (opengui):
-          print('Please wait')
-          time.sleep(1.5)
-          execfile("gui.py")
-          print('Executing command')
-          logging.basicConfig(filename='Previouscommands.log',level=logging.DEBUG)
-          logging.debug(time + username + "has used the open gui command")
-          time.sleep(2)
-          command=input()
-
-  if command == (help):
-          print('admin, mod, laser, upload, opengui, help, exit, version')
-          time.sleep(2)
-          command=input()
-
-  if command == (egg):
-          print('Congrats you have found the easter egg here is your prize!')
-          time.sleep(5)
-          exit()
-
-  if command == (exitprogram):
-          print('Please wait')
-          time.sleep(1.5)
-          print('Exiting Vitanoxi HomeGuard Console')
-          print('Exiting in 5')
-          time.sleep(1)
-          print('4')
-          time.sleep(1)
-          print('3')
-          time.sleep(1)
-          print('2')
-          time.sleep(1)
-          print('1')
-          time.sleep(1)
-          exit()
-
-  if command !=(laser + light + upload + opengui + help + egg + exitprogram):
-          print('Please Wait')
-          time.sleep(1.5)
-          print('No Such Command Please Try Again')
-          time.sleep(2)
-          command=input()
-          
-_mainloop_()
+if command == (exitprogram):
+        print('Please wait')
+        time.sleep(1.5)
+        print('Exiting Vitanoxi HomeGuard Console')
+        print('Exiting in 5')
+        time.sleep(1)
+        print('4')
+        time.sleep(1)
+        print('3')
+        time.sleep(1)
+        print('2')
+        time.sleep(1)
+        print('1')
+        time.sleep(1)
+        exit("Closed")
+        
+if command !=(laser or light or upload or opengui or help or egg or exitprogram or version):
+        print('Please Wait')
+        time.sleep(1.5)
+        print('No Such Command Please Try Again')
+        time.sleep(2)
+        command=input()
+        
+        
