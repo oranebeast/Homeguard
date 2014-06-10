48c50f65c92886244eb9b136176b296db849cb1e
BryceHas2Bubbles <brycesimpson99@gmail.com>
2014-06-10 12:35:45 +1200

    Delete workshop.py

--- modules/workshop.py
+++
@@ -1,8 +0,0 @@
-import time
-import picamera
-
-with picamera.PiCamera() as camera:
-    camera.start_preview()
-    time.sleep(5)
-    camera.capture('/home/pi/Desktop/image.jpg')
-    camera.stop_preview()
