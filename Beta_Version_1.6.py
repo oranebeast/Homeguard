4cac771b24910241ca3bd82e0d86c0008e1a619f
BryceHas2Bubbles <brycesimpson99@gmail.com>
2014-06-13 12:13:22 +1200

    Update Beta_Version_1.6.py

--- Beta_Version_1.6.py
+++ Beta_Version_1.6.py
@@ -1,4 +1,111 @@
+# Copyright (C) 2014 Vitanoxi <Vitanoxi@gmail.com>
+
+#Login
+#  _    _                                                _ 
+# | |  | |                                              | |
+# | |__| | ___  _ __ ___   ___  __ _ _   _  __ _ _ __ __| |
+# |  __  |/ _ \| '_ ` _ \ / _ \/ _` | | | |/ _` | '__/ _` |
+# | |  | | (_) | | | | | |  __/ (_| | |_| | (_| | | | (_| |
+# |_|  |_|\___/|_| |_| |_|\___|\__, |\__,_|\__,_|_|  \__,_|
+#                               __/ |                      
+#                              |___/                 
+
+#import cv2
+#import sys
+#import hashlib
+#import getpass
+import time
+import logging
+import datetime
+import termcolor
+from termcolor import colored
+
+#Variables
+now = datetime.datetime.now()
+#localtime = time.asctime( time.localtime(time.time()) )
+enterpassword='pass'
+version1='HomeGuard Version 1.1'
+laser='laser'
+light='light'
+upload='upload'
+opengui='opengui'
+help='help'
+egg='egg'
+exitprogram='exitprogram'
+version='version'
+time = print now.strftime("%Y-%m-%d %H:%M")
+current_version="1.5"
+developers="Jack Heikell, Bryce Simpson and Sabian Coomber-Nickerson"
+credits="credits"
+
+#Startup sequence
+print('#####################################################################')
+print('                              HOMEGUARD                              ')
+print('                              (C) 2014                               ')
+print(                             'Version ' + current_version              )
+print('#####################################################################')
+time.sleep(1)
+print("Loading 10%")
+time.sleep(0.25)
+print("Loading 20%")
+time.sleep(0.25)
+print("Loading 30%")
+time.sleep(0.25)
+print("Loading 40%")
+time.sleep(0.25)
+print("Loading 50%")
+time.sleep(0.25)
+print("Loading 60%")
+time.sleep(0.25)
+print("Loading 70%")
+time.sleep(0.25)
+print("Loading 80%")
+time.sleep(0.25)
+print("Loading 90%")
+time.sleep(0.25)
+print("Loading 99%")
+time.sleep(5)
+print("Loaded Vitanoxi Verstion " + current_version)
+time.sleep(1)
+print('Please enter your Password.')
+password=input()
+
+if password == (enterpassword):
+        print('Please wait')
+        time.sleep(2)
+        print('')
+        print('Correct, logging in.')
+        time.sleep(1.5)
+        print('Welcome User')
+        time.sleep(2)
+#Logs people that have logged into the console
+        logging.basicConfig(filename='Logging/log.log',level=logging.DEBUG)
+        logging.debug("User logged in at " + time)
+
+
+if password != (enterpassword):
+        print('Please wait')
+        time.sleep(2)
+        print('')
+        print('Incorrect, closing program.')
+        time.sleep(1.5)
+        exit("Closed")
+        
+#Group Command Structure:
+#Admin
+#Mod
+#User
+
+#executes commands when entered
+print('Welcome to your HomeGuard command console by Vitanoxi')
+print('Please input your command below.')
+
+command=input()
+
+#Put this in to make it simple
+        
 if command == (laser or light or upload or opengui or help or egg or exitprogram or version or credits):
         print('Success!')
         else
         print('It doesnt work!')
+                  
