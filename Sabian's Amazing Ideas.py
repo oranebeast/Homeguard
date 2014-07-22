9728db037c1994064d15ddcfa18b456b974cdb3a
miningworm <miningworm@gmail.com>
2014-07-22 14:36:47 +1200

    This is amazing
    Ikr!

---
+++ Sabian's Amazing Ideas.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2014 Vitanoxi <Vitanoxi@gmail.com>
+#  _    _                                                _ 
+# | |  | |                                              | |
+# | |__| | ___  _ __ ___   ___  __ _ _   _  __ _ _ __ __| |
+# |  __  |/ _ \| '_ ` _ \ / _ \/ _` | | | |/ _` | '__/ _` |
+# | |  | | (_) | | | | | |  __/ (_| | |_| | (_| | | | (_| |
+# |_|  |_|\___/|_| |_| |_|\___|\__, |\__,_|\__,_|_|  \__,_|
+#                               __/ |                      
+#                              |___/                 
+
+
+         
+if command == (reload):
+time.sleep(1)
+print("Loading 10%")
+time.sleep(0.25)
+print("Loading 20%")
+time.sleep(0.25)
+print("Loading 30%")
+time.sleep(0.25)
+print("Loading 40%")
+time.sleep(0.25)
+print("Loading 50%")
+time.sleep(0.25)
+print("Loading 60%")
+time.sleep(0.25)
+print("Loading 70%")
+time.sleep(0.25)
+print("Loading 80%")
+time.sleep(0.25)
+print("Loading 90%")
+time.sleep(0.25)
+print("Loading 99%")
+time.sleep(5)
+print("Loaded Vitanoxi Verstion " + current_version)
+time.sleep(1)
+
+        
