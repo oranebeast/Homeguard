342a3a0c829022979c216095d52d926f63aba9e4
oranebeast <oranebeastxboxlive@gmail.com>
2014-07-22 14:48:10 +1200

    Update dropbox_shorts.py

--- dropbox_shorts.py
+++ dropbox_shorts.py
@@ -19,5 +19,21 @@ code = raw_input("Enter the authorization code here: ").strip()
 client = dropbox.client.DropboxClient(access_token)
 print 'Linked account: ', client.account_info()
 print 'Is the linked account correct?, Input YES or NO'
-awnser = raw_input("Your awnser: ").strip()
-print awnser
+awnser=input()
+if awnser = 'YES' :
+  print 'Authorized'
+if awnser = 'NO' :
+  print 'Restarting authorization process'
+  flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+  authorize_url = flow.start()
+  print '1. Go to: ' + authorize_url
+  print '2. Click "Allow" (you might have to log in first)'
+  print '3. Copy the authorization code.'
+  code = raw_input("Enter the authorization code here: ").strip()
+  client = dropbox.client.DropboxClient(access_token)
+  print 'Linked account: ', client.account_info()
+  print 'Is the linked account correct?, Input YES or NO'
+  awnser=input()
+  if awnser = 'NO' :
+    'Restart the application and try again, Also make sure you have a connection to the internet"
+
