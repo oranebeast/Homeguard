52da53848eccf482a96cfc14ca7dd06c655a985a
BryceHas2Bubbles <brycesimpson99@gmail.com>
2014-08-18 10:16:13 +1200

    Added working loop!!!!

--- Beta_Version_1.7.py
+++ Beta_Version_1.7.py
@@ -16,6 +16,10 @@
 import time
 import logging
 
+#########LOOP DO NOT TOUCH!!!#########
+Loop="False"                         #
+#########LOOP DO NOT TOUCH!!!#########
+
 #Variables
 localtime = time.asctime( time.localtime(time.time()) )
 dapassword="pass"
@@ -65,7 +69,7 @@ if beta != ("True"):
         time.sleep(0.25)
         print("Loading 99%")
         time.sleep(5)
-#Third Chance        
+#First Chance        
 if beta != ("True"):
         print("Loaded Vitanoxi Verstion " + current_version)
         time.sleep(1)
@@ -80,6 +84,7 @@ if beta != ("True"):
                 time.sleep(2)
                 logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
                 logging.debug("User logged in at: " + localtime)
+                Loop="True"
         if password == (adminpass):
                 Admin="True"
                 print('Please wait')
@@ -89,7 +94,8 @@ if beta != ("True"):
                 print('Welcome Admin')
                 time.sleep(2)
                 logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-                logging.debug("Admin logged in at: " + localtime)        
+                logging.debug("Admin logged in at: " + localtime)
+                Loop="True"
         if password != (dapassword or adminpass):
                         print('Please wait')
                         time.sleep(2)                        
@@ -109,6 +115,7 @@ if beta != ("True"):
                 time.sleep(2)
                 logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
                 logging.debug("User logged in at: " + localtime)
+                Loop="True"
         if password == (adminpass):
                 Admin="True"
                 print('Please wait')
@@ -118,7 +125,8 @@ if beta != ("True"):
                 print('Welcome Admin')
                 time.sleep(2)
                 logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-                logging.debug("Admin logged in at: " + localtime)        
+                logging.debug("Admin logged in at: " + localtime)
+                Loop="True"
         if password != (dapassword or adminpass):
                         print('Please wait')
                         time.sleep(2)
@@ -138,6 +146,7 @@ if beta != ("True"):
                 time.sleep(2)
                 logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
                 logging.debug("User logged in at: " + localtime)
+                Loop="True"
         if password == (adminpass):
                 Admin="True"
                 print('Please wait')
@@ -147,7 +156,8 @@ if beta != ("True"):
                 print('Welcome Admin')
                 time.sleep(2)
                 logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-                logging.debug("Admin logged in at: " + localtime)        
+                logging.debug("Admin logged in at: " + localtime) 
+                Loop="True"
         if password != (dapassword or adminpass):
                         print('Please wait')
                         time.sleep(2)
@@ -161,155 +171,104 @@ if beta != ("True"):
 #Mod
 #User
 
-#while loop == ("True")
+
 ###########################################COMMANDS#####################################################################
-#executes commands when entered
-if beta == ("False"):
-        print('Welcome to your HomeGuard command console by Vitanoxi')
-        print('Please input your command')
-        else
-        print('Welcome Developer')
-        print('Please input your command')
-if Admin == ("True"):
-        print('Welcome to Admin. You are in control)
-        print('Please input your command')
-        
-#Below is the loop which is broken
+while Loop == ("True")
+        #executes commands when entered
+        if beta == ("False"):
+                print('Welcome to your HomeGuard command console by Vitanoxi')
+                print('Please input your command')
+                else
+                print('Welcome Developer')
+                print('Please input your command')
+                if Admin == ("True"):
+                        print('Welcome to Admin. You are in control)
+                        print('Please input your command')
+                        
+        #Below is the loop which is broken
+        ''''
+        #def _mainloop_():
+        '''
+        command=input()
+        #Admin commands
+        if command == ("log") and Admin ==("True"):
+                print file.read()
+                if command == ("help") and Admin == ("True"):
+                        print('Commands:')
+                        print('mod\nlaser\nupload\nopengui\nhelp\nexitprogram\nversion')
+                        print('')
+                        print('Admin Commands:')
+                        print('log\nhelp')
+                        logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
+                        logging.debug("Someone has used the help command: " + localtime)
+                        time.sleep(2)
+                        command=input()        
+                        #User commands
+                        
+                        if command == ("opengui"):
+                                print('Please wait')
+                                time.sleep(1.5)
+                                execfile("dropbox_shorts.py")
+                                print('Executing command')
+                                logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
+                                logging.debug("Someone has used the open gui command: "  + localtime)
+                                time.sleep(2)
+                                command=input()
+                        if command == ("help") and Admin == ("False"):
+                                print('Commands:')
+                                print('admin\nmod\nlaser\nupload\nopengui\nhelp\nexitprogram\nversion')
+                                logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
+                                logging.debug("Someone has used the help command: " + localtime)
+                                time.sleep(2)
+                                command=input()
+                        if command == ("egg"):
+                                print('Congrats you have found the easter egg here is your prize!')
+                                logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
+                                logging.debug("SOMEONE HAS FOUND THE EASTER EGG! ")
+                                time.sleep(5)
+                                exit()
+                        if command == ("exitprogram"):
+                                print('Please wait')
+                                time.sleep(1.5)
+                                print('Exiting Vitanoxi HomeGuard Console')
+                                print('Exiting in 5')
+                                time.sleep(1)
+                                print('4')
+                                time.sleep(1)
+                                print('3')
+                                time.sleep(1)
+                                print('2')
+                                time.sleep(1)
+                                print('1')
+                                time.sleep(1)
+                                exit("Closed")
+                        if command == ("info"):
+                                print('#####################################################################')
+                                print('                              HOMEGUARD                              ')
+                                print('                              (C) 2014                               ')
+                                print(                               'Version ' + current_version            )
+                                print('                                                                     ')
+                                print('CEO/JrDev: Sabian Coomber-Nickerson                                  ')
+                                print('Project Manager/Developer: Bryce Simpson                             ')
+                                print('Developer: Jack Heikel                                               ')
+                                print('#####################################################################')
+                        if command != ("opengui" or "help" or "egg" or "exitprogram" or "info") and Developer != ("True"):
+                                print('Incorrect command input!')
+                                #End="True"
+                        if command != ("opengui" or "help" or "egg" or "exitprogram" or "info") and Developer == ("True"):
+                                print('NIGGA YOU FAIL AT LIFE!')
+                                #End="True"
 ''''
-#def _mainloop_():
+                        if End == ("True"):
+                                time.sleep(5)
+                                print('Due to tech problems we regret to inform you that you will have to restart the program!')
+                                time.sleep(2)
+                                print('Exit? Y or N')
+                                exit=input()
+                        if exit == ("Y" or "yes" or "y" or "Yes"):
+                                print('Closing')
+                                time.sleep(2)
+                                exit("Closed")
+                        if exit == ("N" or "no" or "n" or "No"):
+                                print('Ok. Thats fine do it yourself!')
 '''
-
-command=input()
-
-#Admin commands
-if command == ("log") and Admin ==("True"):
-        print file.read()
-if command == ("help") and Admin == ("True"):
-print('Commands:')
-        print('mod\nlaser\nupload\nopengui\nhelp\nexitprogram\nversion')
-        print('')
-        print('Admin Commands:')
-        print('log\nhelp')
-        logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-        logging.debug("Someone has used the help command: " + localtime)
-        time.sleep(2)
-        command=input()        
-
-#User commands
-
-if command == ("laser"):
-        print('Please wait')
-        time.sleep(1.5)
-        execfile("laser.py")
-        print('Executing command')
-        logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-        logging.debug("Someone has used the laser command: " + localtime)
-        time.sleep(2)
-        command=input()
-        
-if command == ("light"):
-        print('Please wait')
-        time.sleep(1.5)
-        execfile("light.py")
-        print('Executing command')
-        logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-        logging.debug("Someone has used the light command: " + localtime)
-        time.sleep(2)
-        command=input()
-        
-if command == ("upload"):
-        print('Please wait')
-        time.sleep(1.5)
-        execfile("upload.py")
-        print('Executing command')
-        logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-        logging.debug("Someone has used the upload command: "  + localtime)
-        time.sleep(2)
-        command=input()
-
-if command == ("opengui"):
-        print('Please wait')
-        time.sleep(1.5)
-        execfile("dropbox_shorts.py")
-        print('Executing command')
-        logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-        logging.debug("Someone has used the open gui command: "  + localtime)
-        time.sleep(2)
-        command=input()
-
-if command == ("help") and Admin == ("False"):
-        print('Commands:')
-        print('admin\nmod\nlaser\nupload\nopengui\nhelp\nexitprogram\nversion')
-        logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-        logging.debug("Someone has used the help command: " + localtime)
-        time.sleep(2)
-        command=input()
-
-if command == ("egg"):
-        print('Congrats you have found the easter egg here is your prize!')
-        logging.basicConfig(filename='Logging/logs.log',level=logging.DEBUG)
-        logging.debug("SOMEONE HAS FOUND THE EASTER EGG! ")
-        time.sleep(5)
-        exit()
- 
-if command == ("exitprogram"):
-        print('Please wait')
-        time.sleep(1.5)
-        print('Exiting Vitanoxi HomeGuard Console')
-        print('Exiting in 5')
-        time.sleep(1)
-        print('4')
-        time.sleep(1)
-        print('3')
-        time.sleep(1)
-        print('2')
-        time.sleep(1)
-        print('1')
-        time.sleep(1)
-        exit("Closed")
-
-if command == ("info"):
-        print('#####################################################################')
-        print('                              HOMEGUARD                              ')
-        print('                              (C) 2014                               ')
-        print(                               'Version ' + current_version            )
-        print('                                                                     ')
-        print('CEO/JrDev: Sabian Coomber-Nickerson                                  ')
-        print('Project Manager/Developer: Bryce Simpson                             ')
-        print('Developer: Jack Heikel                                               ')
-        print('#####################################################################')
-        
-if command != ("laser" or "light" or "upload" or "opengui" or "help" or "egg" or "exitprogram" or "info") and Developer != ("True"):
-        print('Incorrect command input!')
-        End="True"
-
-if command != ("laser" or "light" or "upload" or "opengui" or "help" or "egg" or "exitprogram" or "info") and Developer == ("True"):
-        print('NIGGA YOU FAIL AT LIFE!')
-        End="True"
-        
-if End == ("True"):
-        time.sleep(5)
-        print('Due to tech problems we regret to inform you that you will have to restart the program!')
-        time.sleep(2)
-        print('Exit? Y or N')
-        exit=input()
-        if exit == ("Y" or "yes" or "y" or "Yes"):
-                print('Closing')
-                time.sleep(2)
-                exit("Closed")
-        if exit == ("N" or "no" or "n" or "No"):
-                print('Ok. Thats fine do it yourself!')
-                
-        
-        
-        
-        
-
-        
-        
-
-        
-
-        
-        
-        
